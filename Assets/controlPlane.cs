b968a70f0f4c6eb90b2ec4d4dd22026d5217af02
a3doucet <alison.doucette@ryerson.ca>
2017-06-24 15:09:31 -0400

    Basic Setup

---
+++ Assets/controlPlane.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controlPlane : MonoBehaviour {
+
+    public GameObject plane;
+    float v, h;
+	// Update is called once per frame
+	void Update () {
+        v = Input.GetAxis("Vertical") * 0.1f;
+        h = Input.GetAxis("Horizontal") * 0.25f;
+        plane.transform.position = new Vector3(plane.transform.position.x, plane.transform.position.y + v, plane.transform.position.z);
+        plane.transform.eulerAngles = new Vector3(plane.transform.eulerAngles.x, plane.transform.eulerAngles.y, plane.transform.eulerAngles.z + h);
+
+	}
+}
